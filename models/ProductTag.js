ca1fe5bb31418ec3798731265db67c45de2ea6ba
MarcM <marina.ved80gmail.com>
2024-09-01 23:31:23 -0600

    add models data

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,8 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   // DONE: define columns
   {
-    
-    
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This references to the `product` model, which has `id`
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This references to the `tag` model, which has `id`
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
